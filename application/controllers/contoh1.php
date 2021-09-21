e904a2ce6196cf7afe4e0fc3746f4fc5ca5f5f72
zwzonepieces <riskimpw9988@gmail.com>
2021-09-21 22:13:40 +0700

    perubahan index

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -4,9 +4,9 @@ class Contoh1 extends CI_Controller
  public function index()
  {
  echo "<h1>Perkenalkan</h1>";
- echo"
- Saya tingga di daerah Ciputat
+ echo"Nama saya Rizki Lutfiandi
+ Saya tinggal di daerah pelosok
  olah raga yang saya sukai adalah
- Bulutangkis";
+ Volly Ball";
  }
 }
