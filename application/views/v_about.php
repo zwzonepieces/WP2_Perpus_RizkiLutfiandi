01997ff8544595e28d28062e07dde86402677ee6
zwzonepieces <riskimpw9988@gmail.com>
2021-09-27 22:44:46 +0700

    commit tugas 3

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Rizki</li>
+        <li>Nama Belakang : Lutfiandi</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> JL.MANUNGGAL XIII DESA SUI KUNYIT HULU</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>SUI KUNYIT HULU</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+       <li>Volli</li>
+       <li>Catur</li>
+    </ul>
+</section>
---
+++ application/views/v_footer.php
@@ -0,0 +1,6 @@
+        <footer>
+            <a href="http://www.bsi.ac.id">UBSIPONTIANAK</a>
+        </footer>
+    </div>
+</body>
+</html>
