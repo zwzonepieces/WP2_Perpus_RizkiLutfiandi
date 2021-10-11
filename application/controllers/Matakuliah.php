d6279c49929a46e6fade0ad7d74302b40977aab0
zwzonepieces <riskimpw9988@gmail.com>
2021-10-11 13:54:23 +0700

    Commit Tugas Ke-5

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -30,7 +30,7 @@ class Matakuliah extends CI_Controller
                 'kode' => $this->input->post('kode'),
                 'nama' => $this->input->post('nama'),
                 'sks' => $this->input->post('sks')
-            ];
+            ]; 
             $this->load->view('view-data-matakuliah', $data);
         }
     }
